b6fbe2eda4404f42701ab9b5beff4ee33bf63ee7
Gabriel Silva <63379300+GABRIEL08266644@users.noreply.github.com>
2021-03-12 08:40:58 -0300

    php basico

---
+++ basico/comentarios.php
@@ -0,0 +1,23 @@
+<div class="titulo">Comentarios PHP</div>
+
+<?php
+echo "Estou no PHP";
+// echo "Comentario";
+#echo "Comentario";
+#echo //"Comentario";
+// echo #"Comentario";
+// ?>
+
+<p>Depois do Primeiro bloco!</p>
+<!-- <p>Comentário HTML1</p> -->
+<!-- <p>Comentário HTML2</p> -->
+<!-- <p>Comentário HTML3</p> -->
+
+<?php
+/*
+    Linha 1 do comentário
+    Linha 2 do comentário
+    Linha 3 do comentário
+*/
+?>
+<p>depois do segundo bloco!</p>
--- index.php
+++ index.php
@@ -18,7 +18,23 @@
             <nav class="modulos">
                 <div class="modulo verde">
                     <h3>Módulo 01</h3>
-                  
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=ola">Olá php</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=html">Integração HTML</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=css">Integração CSS</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">Comentarios PHP</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=desafio">Desafio</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </div>
